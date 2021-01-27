0e1b65dd4953700a23f059e1938d7dcb731c808c
kymko <Kymkobek@gmail.com>
2021-01-27 23:08:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double[] array = {1.1, 1.2, 2.4, 5.5, 10.3, -15.6, 10.1, 10.9, -15.3, -30.4, 5.3, 6.9, 12.3, 18.4, 18.2, -10.3};
+        for (double i:array) {
+            System.out.println(array[6]+array[7]/2);
+            break;
+
+        }
+    }
+}
