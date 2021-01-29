7832cb388fde46097fac895e17b3d74d5685c474
kymko <Kymkobek@gmail.com>
2021-01-29 15:23:57 +0300

    Добавил д/з на сообразительность

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -58,7 +58,6 @@ public class Main {
         for (int i = 0; i < size - 1; i++){
             for (int j = 0; j < size - i - 1; j++){
                 if (array[j] > array[j + 1]) {
-                    // swap if greater is at the rear position
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
